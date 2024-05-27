86d5ea290d7222740f216142b071275305d34c19
ShaneDaniel484 <shanedaniel484@gmail.com>
2024-05-27 21:36:55 +0530

    session 7 completed

--- test/features/demo/inventory.feature
+++ test/features/demo/inventory.feature
@@ -1,10 +1,10 @@
 Feature: Sauce labs price checking feature
 
-
+    @demo
     Scenario Outline: Check wether all products have a valid price
         Given user is logged in sauce labs
-        Then user sees <NumberOfProducts> products listed
-        Then user validates the products have valid price
+        # Then user sees <NumberOfProducts> products listed
+        # Then user validates the products have valid price
         Examples:
             | TestID    | NumberOfProducts |
             | INV_TC001 | 6                |
--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -11,6 +11,40 @@ Given(/^user is logged in sauce labs$/, async function () {
   await $(`#user-name`).setValue("standard_user")
   await $(`#password`).setValue("secret_sauce")
   await $(`#login-button`).click()
+  await browser.pause(2000)
   
   //await browser.debug();
+
+  //log in with another user
+  // await browser.reloadSession()
+  // await browser.url("https://www.saucedemo.com/v1/");
+  // await $(`#user-name`).setValue("problem_user")
+  // await $(`#password`).setValue("secret_sauce")
+  // await $(`#login-button`).click()
+  // await browser.debug()
+
+
+  //try catch
+  // try {
+  //   await $(`#user-nam`).setValue("standard_user")
+  //   await $(`#password`).setValue("secret_sauce")
+  //   await $(`#login-button`).click()
+  //   await browser.pause(2000)
+  // } catch (err) {
+  //   await browser.refresh()
+  //   console.log(`User not logged in....RETRYING....`);
+  //   await $(`#user-name`).setValue("standard_user")
+  //   await $(`#password`).setValue("secret_sauce")
+  //   await $(`#login-button`).click()
+  // }
+  
+
+  //back and forward
+
+  await browser.back()
+  await browser.pause(2000)
+  await browser.forward()
+  await browser.debug()
+
+
 });
